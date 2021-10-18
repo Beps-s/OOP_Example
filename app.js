a3f82c4593a1cd48f0d725011fe35ae101e916b3
UwUBeebi <chris.erman@khk.ee>
2021-10-18 14:24:11 +0300

    OOJS 1

---
+++ app.js
@@ -0,0 +1,18 @@
+function Shape(name, sides, sideLength) {
+	this.name = name;
+	this.sides = sides;
+	this.sideLength = sideLength;
+	this.calcPerimeter = function(){
+		return this.sides * this.sideLength;
+	}
+
+}
+
+let square = new Shape("square", 2, 7);
+let triangle = new Shape("triangle", 1, 3);
+
+console.log(square);
+console.log(square.calcPerimeter());
+
+console.log(triangle);
+console.log(triangle.calcPerimeter());
