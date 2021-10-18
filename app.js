c89c0dfc5612bd74b7a782a614ddc112dddba648
UwUBeebi <chris.erman@khk.ee>
2021-10-18 14:26:08 +0300

    OOJS 2

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-function Shape(name, sides, sideLength) {
-	this.name = name;
-	this.sides = sides;
-	this.sideLength = sideLength;
-	this.calcPerimeter = function(){
+class Shape {
+	constructor(name, sides, sideLength) {
+		this.name = name;
+		this.sides = sides;
+		this.sideLength = sideLength;
+		}
+
+	calcPerimeter(){
 		return this.sides * this.sideLength;
 	}
-
 }
 
 let square = new Shape("square", 2, 7);
